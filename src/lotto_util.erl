c2513b9ad8b0f0f468107e07a9a1bda00714545b
Steve King <IW-SteveK@Steves-MacBook-Pro.local>
2016-11-30 13:17:41 +0000

    fixed random ticket geerator.

--- src/lotto_util.erl
+++ src/lotto_util.erl
@@ -4,5 +4,5 @@
 
 
 ticket(ID) ->
-    Ticket = lists:map(fun() -> rand:uniform(10) end, lists:seq(1, 6)),
+    Ticket = lists:map(fun(_) -> rand:uniform(10) end, lists:seq(1, 6)),
     lotto_sup:start_ticket(ID, Ticket).
